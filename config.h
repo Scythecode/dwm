e9ee4b5e4bf63c1b66c1393a2d6cb8a0a22d544a
sam <sammie.dev@proton.me>
2025-02-19 16:36:45 -0500

    fixed compiler error, caused by  being commented. meaning that  is no longer needed

--- config.h
+++ config.h
@@ -23,8 +23,8 @@ static const int showsystray             = 1;   /* 0 means no systray */
 static int tagindicatortype              = INDICATOR_TOP_LEFT_SQUARE;
 static int tiledindicatortype            = INDICATOR_NONE;
 static int floatindicatortype            = INDICATOR_TOP_LEFT_SQUARE;
-static const char *fonts[]               = { "monospace:size=10" };
-static const char dmenufont[]            = "monospace:size=10";
+static const char *fonts[]               = { "Inconsolata:size=10" };
+static const char dmenufont[]            = "Inconsolata:size=10";
 
 static char c000000[]                    = "#000000"; // placeholder value
 
--- make.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo make clean install LIBS="-L/usr/X11R6/lib -lX11 -lXinerama -lfontconfig -lXft -lXrender"
-
